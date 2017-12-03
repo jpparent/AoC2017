0a5ed53fe98fa46266b6eefd671da1e9a6be3f6e
jpparent <jeanp.parent@gmail.com>
2017-12-03 15:07:51 -0500

    Day 02

---
+++ 02-1.py
@@ -0,0 +1,11 @@
+f = open('02-1.input')
+rows = f.readlines()
+f.close()
+
+checksum = 0
+
+for row in rows:
+	values = [int(x) for x in row.split()]
+	checksum += max(values) - min(values)
+
+print checksum
---
+++ 02-2.py
@@ -0,0 +1,16 @@
+f = open('02-1.input')
+rows = f.readlines()
+f.close()
+
+sum = 0
+
+for row in rows:
+	values = [int(x) for x in row.split()]
+	
+	for i in range(0,len(values)):
+		current = values[i]
+		for j in range(0, len(values)):
+			if i != j and current % values[j] == 0:
+				sum += current / values[j]
+
+print sum
