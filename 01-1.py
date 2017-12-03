249ce1d447b1add1b97b83d40731021c37d101d2
jpparent <jeanp.parent@gmail.com>
2017-12-03 14:33:15 -0500

    Day 01

---
+++ 01-1.py
@@ -0,0 +1,22 @@
+f = open('01-1.input')
+inputs = f.read()
+f.close()
+
+sum = 0
+
+matches = []
+
+last = 0
+for i in inputs:
+	if i == last:
+		matches.append(i)
+	last = i
+
+#loop for the last one
+if i == inputs[0]:
+	matches.append(i)
+
+for p in matches:
+	sum = sum + int(p)
+
+print sum
---
+++ 01-2.py
@@ -0,0 +1,22 @@
+f = open('01-1.input')
+inputs = f.read()
+f.close()
+
+sum = 0
+length = len(inputs)
+halfLength = length/2
+
+matches = []
+
+compare = 0
+for i in range(0, length):
+	current = inputs[i]
+	compare = inputs[(i + halfLength) % length]
+	
+	if current == compare:
+		matches.append(current)
+
+for p in matches:
+	sum = sum + int(p)
+
+print sum
